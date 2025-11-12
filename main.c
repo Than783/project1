24cf5ecf99b1df4bab92dcd81949b8291e6ee591
ioannes bogiatzioglou <mpogias123450@gmail.com>
2025-11-12 20:07:11 +0200

    commit

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  printf("Thanasis\n");
   return 0;
 }
--- main2.c
+++ main2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello 2025\n");
+  printf("Athanasiou\n");
   return 0;
 }
